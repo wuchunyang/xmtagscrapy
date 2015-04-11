8149714327cac4b7bb6a6a6c401467f6b7a17798
CHUNYANG <cywindor@gmail.com>
2015-04-11 22:57:51 +0800

    commit
    recommit

--- flaskxm.py
+++ flaskxm.py
@@ -1,2 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+from flask import Flask
+from flask import request
+import json
+import xiamiscrapy
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+@app.route('/searchtags/')
+def hello_world():
+
+    if request.method == 'GET':
+        return json.dumps(xiamiscrapy.search(request.args.get('song',''), request.args.get('singer','')))
+
+    return 'oops,something was wrong!'
+
+@app.route('/user/<username>')
+def user(username):
+    return 'hello %s' % username
+
+@app.route('/id/<int:num>/')
+def hid(num):
     return 'num %d' % num
+
+if __name__ == '__main__':
 //    app.debug = True
+    app.run()
