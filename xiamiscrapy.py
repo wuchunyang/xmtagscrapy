96663ce2ebe9bae9cd7ea2d121c3ffbc8e905e5e
CHUNYANG <cywindor@gmail.com>
2015-04-14 00:15:49 +0800

    add also like

--- xiamiscrapy.py
+++ xiamiscrapy.py
@@ -32,7 +32,7 @@ def search(song,singer):
     print detailre
     
     if(detailre == []):
-        print detailre
+#         print detailre
         
         return {"status":"-1","message":"song not found"}
     
@@ -42,18 +42,32 @@ def search(song,singer):
         tagrequest.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36')
         tagresponse = urllib2.urlopen(tagrequest)
         taghtml = tagresponse.read()
-#         print html
+        print searchtagurl
+#         print taghtml
         tagdetailre = re.findall(r'title="标签为(\S*?)的歌曲">' ,taghtml,re.S)
+        
+        alsolisten = re.findall(r'听'+song.encode('utf-8')+'的人也听(.*?)</table>', taghtml, re.S)
+        
+        
+        alsolistensong = re.findall(r'<td class="song_name">(.*?)">(.*?)</a></p>', alsolisten[0], re.S)
+        
+        alsolistensongs = []
+        
+        if((alsolisten !=[]) and (alsolistensong != [])):
+            for i in range(len(alsolistensong)):
+                alsolistensongs.append(alsolistensong[i][1].replace("\r\n","").replace("\t",""))
+                
+
         
         if(tagdetailre == []):
             return {"status":"-2","message":"no tags"}
         else:
-            return {"status":"1","message":"ok","tags":tagdetailre}
+            return {"status":"1","message":"ok","tags":tagdetailre,"similar":alsolistensongs}
             
 
 
 if __name__ == '__main__':
-#     search(u'好久不见',u'陈奕迅')    
-    search(u'People Need Love',u'ABBA')
+    search(u'好久不见',u'陈奕迅')    
+#     search(u'Ring Ring',u'ABBA')
     
     
